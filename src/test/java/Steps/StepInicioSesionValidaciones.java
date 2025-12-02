d4ba6d25da50aef9c6e2cb520e7f1368613201f2
VanesaAraujo <harold.horta@zabud.com.co>
2025-12-02 17:35:04 -0500

    Prueba funcionando de Validaciones_de_Inicio_de_sesion.png

--- src/test/java/Steps/StepInicioSesionValidaciones.java
+++ src/test/java/Steps/StepInicioSesionValidaciones.java
@@ -11,31 +11,56 @@ import static driverSetup.driver.page;
 public class StepInicioSesionValidaciones {
     @Given("El usuario accede al sistema de upccelerator")
     public void el_usuario_accede_al_sistema_de_upccelerator() {
-        page.navigate("https://appedesign.upccelerator.com/login");
+        page.navigate("https://appredesign.upccelerator.com/login");
         System.out.println(page.title());
     }
     @Given("Correo incorrecto")
     public void correo_incorrecto() {
-    page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Correo electrónico Correo")).fill("vanesaqvanesa.com");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Correo electrónico Correo")).fill("vanesavanesa.com");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Enviar código de ingreso")).click();
+        System.out.println("correo incorrecto");
     }
     @When("Correo NO registrado en la base de datos")
     public void correo_no_registrado_en_la_base_de_datos() {
-     page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Correo electrónico Correo")).fill("varaujo123@devcol.com");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Correo electrónico Correo")).fill("varaujo@devcol.co");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Enviar código de ingreso")).click();
+        System.out.println("correo no registrado");
     }
     @When("Correo registrado en la base de datos y clic en enviar codigo de ingreso")
     public void correo_registrado_en_la_base_de_datos_y_clic_en_enviar_codigo_de_ingreso() {
         page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Correo electrónico Correo")).fill("varaujo@devcol.com.co");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Enviar código de ingreso")).click();
+        System.out.println("correo registrado con codigo incorrecto");
+    }
+    @When("Codigo con letras")
+    public void codigo_con_letras() {
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).first().fill("4");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(1).fill("1");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(2).fill("5");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(3).fill("a");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(4).fill("s");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(5).fill("1");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Ingresar")).click();
+        System.out.println("código con letras");
     }
     @When("Codigo incorrecto")
     public void codigo_incorrecto() {
-
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).first().fill("5");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(1).fill("4");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(2).fill("6");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(3).fill("6");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(4).fill("6");
+        page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Código de Acceso")).nth(5).fill("5");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Ingresar")).click();
+        System.out.println("codio incorrecto");
     }
     @When("Reenviar codigo")
     public void reenviar_codigo() {
-
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("reenviar")).click();
+        System.out.println("Reenviar código");
     }
     @Then("Codigo correcto")
     public void codigo_correcto() {
-        throw new io.cucumber.java.PendingException();
+
     }
 }
